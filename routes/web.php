d8972b0a1be6d68a151e83f296daecf32461dea1
Laura Lima <limalausantoss@gmail.com>
2026-08-24 14:46:05 -0300

    wip: introdução orm

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,13 @@ Route::get('/', function () {
 
 Route::get('/produtos', [ProdutoController::class , 'index']);
 Route::post('/produtos', [ProdutoController::class , 'store']);
+
+Route::get('/teste-orm', function() {
+    User::create([
+        'name' => 'Ana Clara Santos',
+        'email' => 'ana.santos@escola.sp.gov.br',
+        'password' => '12345678'
+    ]);
+
+    return User::all();
+});
